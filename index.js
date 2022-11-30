12f17b6de3267a2eda6c838bd9900f38ac9b0081
Seda Ünal <sedaylmz06@outlook.com>
2022-12-01 00:33:41 +0300

    dom editing

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here!
+const element = document.createElement("div");
+document.body.append(element);
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+  li.textContent = "Hi there!";
+}
+
+element.append(ul);
+const main = document.getElementById("main");
+main.innerHTML =
+  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+// element.textContent = "You've changed what's on the screen!";
+element.style.fontSize = "24px";
+element.style.marginLeft = "30px";
+element.style.lineHeight = 2;
+
+element.className = "pet-listing dog";
+element.classList.remove("dog");
+element.classList.add("cat", "sale");
+
+const secondChild = ul.querySelector("li:nth-child(2)");
+ul.removeChild(secondChild);
